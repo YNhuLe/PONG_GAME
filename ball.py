0c388207e4479c2963b6ac93f62fb9b35fc51cfa
jennyle96 <154112344+jennyle96@users.noreply.github.com>
2023-12-19 22:51:01 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -4,7 +4,7 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.color("white")
+        self.color("blue")
         self.shape("circle")
         self.penup()
         self.x_move = 10
